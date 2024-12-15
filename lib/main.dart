2e5e1a80d8767834c56b974f35b10fd6cf4e2f68
mooN2Night <gocha010997@mail.ru>
2024-12-15 16:02:40 +0400

    Loan rate calculation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -13,113 +14,201 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const CreditCalculatorHomePage(title: 'Credit Calculator'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class CreditCalculatorHomePage extends StatefulWidget {
+  const CreditCalculatorHomePage({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<CreditCalculatorHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePageState extends State<CreditCalculatorHomePage> {
+  final _formKey = GlobalKey<FormState>();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  late final TextEditingController _loanAmountController;
+  late final TextEditingController _interestRateController;
+  late final TextEditingController _loanTermController;
+
+  String _paymentType = 'Аннуитетный';
+  final List<String> _paymentTypeList = ['Аннуитетный', 'Дифференцированный'];
+  double? _monthlyPayment;
+  double? _totalPayment;
+  double? _overpayment;
+
+  @override
+  void initState() {
+    super.initState();
+    _loanAmountController = TextEditingController();
+    _interestRateController = TextEditingController();
+    _loanTermController = TextEditingController();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        child: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _loanAmountController,
+                  decoration: const InputDecoration(labelText: 'Сумма кредита'),
+                  keyboardType: TextInputType.number,
+                  validator: _loanAmountValidator,
+                ),
+                TextFormField(
+                  controller: _interestRateController,
+                  decoration: const InputDecoration(
+                    labelText: 'Процентная ставка (годовая)',
+                  ),
+                  keyboardType: TextInputType.number,
+                  validator: _interestRateValidator,
+                ),
+                TextFormField(
+                  controller: _loanTermController,
+                  decoration: const InputDecoration(
+                    labelText: 'Срок кредита (в месяцах)',
+                  ),
+                  keyboardType: TextInputType.number,
+                  validator: _loanTermValidator,
+                ),
+                DropdownButtonFormField(
+                  value: _paymentType,
+                  decoration: const InputDecoration(labelText: 'Тип платежа'),
+                  items: _paymentTypeList
+                      .map(
+                        (type) => DropdownMenuItem(
+                          value: type,
+                          child: Text(type),
+                        ),
+                      )
+                      .toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      _paymentType = value!;
+                    });
+                  },
+                ),
+                const SizedBox(height: 20),
+                if (_monthlyPayment != null && _totalPayment != null && _overpayment != null)
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Ежемесячный платеж: ${_monthlyPayment!.toStringAsFixed(2)} ₽'),
+                      Text('Общая сумма выплат: ${_totalPayment!.toStringAsFixed(2)} ₽'),
+                      Text('Переплата: ${_overpayment!.toStringAsFixed(2)} ₽'),
+                    ],
+                  ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: _calculate,
+        child: const Icon(Icons.calculate_outlined),
+      ),
     );
+  }
+
+  void _calculate() {
+    if (_formKey.currentState!.validate()) {
+      final double loanAmount = double.parse(_loanAmountController.text);
+      final double annualInterestRate =
+          double.parse(_interestRateController.text) / 100;
+      final int loanTermMonths = int.parse(_loanTermController.text);
+      final bool isAnnuity = _paymentType == 'Аннуитетный';
+
+      /// Аннуитетный: Х = С * К, где X — аннуитетный платеж, С — сумма кредита,
+      /// К — коэффициент аннуитета. Коэффициент аннуитета считается так:
+      /// К = (М * (1 + М) ^ S) / ((1 + М) ^ S — 1), где М — месячная процентная
+      /// ставка по кредиту, S — срок кредита в месяцах.
+      if (isAnnuity) {
+        /// месячная процентная ставка
+        final double monthlyInterestRate = annualInterestRate / 12;
+
+        /// коэффициент аннуитета
+        final double annuityFactor = (monthlyInterestRate *
+                pow(1 + monthlyInterestRate, loanTermMonths)) /
+            (pow(1 + monthlyInterestRate, loanTermMonths) - 1);
+
+        _monthlyPayment = loanAmount * annuityFactor;
+
+        /// Дифференцированный формула: DP = s/n + OD * i, OD = s - s/n * m где:
+        /// DP – дифференцированный платеж; s – первоначальная сумма кредита;
+        /// n - количество процентных периодов во всем сроке кредита;
+        /// OD - остаток долга по кредиту на дату расчета DP;
+        /// i - месячная процентная ставка; m - номер текущего месяца
+      } else {
+        /// основная часть платежа - s/n
+        _monthlyPayment = loanAmount / loanTermMonths;
+
+        _totalPayment = 0;
+        for (int i = 0; i < loanTermMonths; i++) {
+          ///  - OD
+          _totalPayment = (_totalPayment ?? 0) +
+              /// общий платеж за месяц
+              _monthlyPayment! +
+              /// проценты на остаток задолженности за каждый месяц - OD
+              (loanAmount - _monthlyPayment! * i) * (annualInterestRate / 12);
+        }
+
+        /// переплата
+        _overpayment = _totalPayment! - loanAmount;
+      }
+
+      if (_monthlyPayment != null) {
+        _totalPayment = _totalPayment ?? _monthlyPayment! * loanTermMonths;
+        _overpayment = _totalPayment! - loanAmount;
+      }
+
+      setState(() {});
+    }
+  }
+
+  String? _loanTermValidator(value) {
+    if (value == null ||
+        value.isEmpty ||
+        int.tryParse(value) == null ||
+        int.parse(value) <= 0) {
+      return 'Введите корректный срок';
+    }
+    return null;
+  }
+
+  String? _interestRateValidator(value) {
+    if (value == null ||
+        value.isEmpty ||
+        double.tryParse(value) == null ||
+        double.parse(value) <= 0) {
+      return 'Введите корректную ставку';
+    }
+    return null;
+  }
+
+  String? _loanAmountValidator(value) {
+    if (value == null ||
+        value.isEmpty ||
+        double.tryParse(value) == null ||
+        double.parse(value) <= 0) {
+      return 'Введите корректную сумму';
+    }
+    return null;
   }
 }
